8fbdc1366b70d1e43eab699b73ff829d6d955db4
Oscar Robertson <86749444+orthesavage@users.noreply.github.com>
2023-01-17 16:57:08 +1300

    Add operate() and call when handling button#equals

--- javascript.js
+++ javascript.js
@@ -7,13 +7,31 @@ function updateScreen() {
     display.textContent = `FN: ${firstNumber}, CN: ${curNumber}, OP: ${operator}`;
 }
 
+function operate() {
+    const a = Number(firstNumber);
+    const b = Number(curNumber);
+
+    if (operator === '+') {
+        const result = a + b;
+    } else if (operator === '-') {
+        const result = a - b;
+    } else if (operator === '\u00D7') {
+        const result = a * b;        
+    } else if (operator === '\u00F7') {
+        const result = a / b;
+    }
+
+    firstNumber = String(result);
+    curNumber = '';
+}
+
 const buttons = document.querySelectorAll('.buttons button');
 buttons.forEach(button => {
     button.addEventListener('click', () => {
         if (!button.classList.contains('operator')) {
             curNumber += button.textContent;
         } else if (button.id === 'equals') {
-            // TODO
+            operate();
         } else {
             operator = button.textContent;
         }
